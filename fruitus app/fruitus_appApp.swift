240f5c800c5a9b1ba41c61b8d68a6b5bfec39afb
David Olagookun <erijesudo@gmail.com>
2023-04-07 06:44:33 +0100

    Initial Commit

---
+++ fruitus app/fruitus_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  fruitus_appApp.swift
+//  fruitus app
+//
+//  Created by David OH on 07/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct fruitus_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
